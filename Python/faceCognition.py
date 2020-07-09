0061c9ae5596c39abd1abbcbf9fbeeca66a457ed
nsc-tai-sakakibara <63280654+nsc-tai-sakakibara@users.noreply.github.com>
2020-07-09 17:17:38 +0900

    顔認識　終了キーメッセージ付き
    Python＼faceCognition.py　が
    日本語で終了(Esc)キーを表示している
    スクリプトです。

--- Python/faceCognition.py
+++ Python/faceCognition.py
@@ -1,100 +1,60 @@
-import sys
 import cv2
+from PIL import Image,ImageFont,ImageDraw
+import numpy as np
 
-cap=cv2.VideoCapture(0)
+#putText の代わりの日本語表示関数
+def imgMsg(img, message):
+    font_path = 'C:\Windows\Fonts\meiryo.ttc'
+    font_size = 24
+    font = ImageFont.truetype(font_path, font_size)
+    img = Image.fromarray(img)
+    draw = ImageDraw.Draw(img)
+    draw.text((50, 50), message, font=font, fill=(0, 0, 255, 0))
+    img = np.array(img)
+    return img
 
-if cap.isOpened() is False:
-    print("can not open camera")
-    sys.exit()
+#opencv 顔と目の認識用ファイル
+faceCascadePath = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+eyeCascadePath = cv2.data.haarcascades + 'haarcascade_eye.xml'
 
-# 評価器を読み込み
-# https://github.com/opencv/opencv/tree/master/data/haarcascades
-cascade = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_alt2.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+faceCascade = cv2.CascadeClassifier(faceCascadePath)
+eyeCascade = cv2.CascadeClassifier(eyeCascadePath)
 
-def mosaic(src, ratio=0.1):
-    small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
-    return cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+#デフォルト
+cam = cv2.VideoCapture(0)
 
-def mosaic_area(src, x, y, width, height, ratio=0.1):
-    dst = src.copy()
-    dst[y:y + height, x:x + width] = mosaic(dst[y:y + height, x:x + width], ratio)
-    return dst
+#メッセージ内容
+message='Esc キーで終了します'
 
 while True:
-    # VideoCaptureから1フレーム読み込む
-    ret, frame = cap.read()
+    ret, img = cam.read()
 
-    # そのままの大きさだと処理速度がきついのでリサイズ
-    frame = cv2.resize(frame, (int(frame.shape[1]*0.7), int(frame.shape[0]*0.7)))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.41, minNeighbors=5)
 
-    # 処理速度を高めるために画像をグレースケールに変換したものを用意
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #認識範囲調整
+    for x, y, w, h in faces:
 
-    # 顔検出
-    facerect = cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.11,
-        minNeighbors=3,
-        minSize=(100, 100)
-    )
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    if len(facerect) != 0:
-        for x, y, w, h in facerect:
-            # 顔の部分(この顔の部分に対して目の検出をかける)
-            face_gray = gray[y: y + h, x: x + w]
+        face = img[y: y + h, x: x + w]
 
-            # くり抜いた顔の部分を表示(処理には必要ない。ただ見たいだけ。)
-            show_face_gray = cv2.resize(face_gray, (int(gray.shape[1]), int(gray.shape[0])))
-            cv2.imshow('face', show_face_gray)
+        faceGray = gray[y: y + h, x: x + w]
+        eyes = eyeCascade.detectMultiScale(faceGray, scaleFactor=1.61, minNeighbors=5)
 
-            # 顔の部分から目の検出
-            eyes = eye_cascade.detectMultiScale(
-                face_gray,
-                scaleFactor=1.11, # ここの値はPCのスペックに依存するので適宜修正してください
-                minNeighbors=3,
-                minSize=(15, 15)
-            )
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(face, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-            if len(eyes) == 0:
-                # 目が閉じられたとみなす
-                cv2.putText(
-                    frame,
-                    'close your eyes',
-                    (x, y - 10), # 位置を少し調整
-                    cv2.FONT_HERSHEY_PLAIN,
-                    2,
-                    (0, 255,0),
-                    2,
-                    cv2.LINE_AA
-                )
-            else:
-                for (ex, ey, ew, eh) in eyes:
-                    # 目の部分にモザイク処理
-                    frame = mosaic_area(
-                        frame,
-                        int((x + ex) - ew / 2),
-                        int(y + ey),
-                        int(ew * 2.5),
-                        eh
-                    )
-
-            # 顔検出した部分に枠を描画
-            cv2.rectangle(
-                frame,
-                (x, y),
-                (x + w, y + h),
-                (255, 255, 255),
-                thickness=2
-            )
-
-    cv2.imshow('frame', frame)
-
-    # キー入力を1ms待って、k が27（ESC）だったらBreakする
-    k = cv2.waitKey(1)
-    if k == 27:
+    #映像出力
+    img = imgMsg(img, message)
+    cv2.imshow('video image', img)
+    
+    #10msec　入力待ち
+    key = cv2.waitKey(10)
+    # ESCキーで終了
+    if key == 27:
         break
 
-# キャプチャをリリースして、ウィンドウをすべて閉じる
-cap.release()
+#キャプチャ終了
+cam.release()
 cv2.destroyAllWindows()
